c38e9dd474e38cc9dec54246908132b18c75174d
zxvri23 <161262364+zxvri23@users.noreply.github.com>
2025-07-06 18:29:09 +0300

    Init project.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.tuvarna.bg.library {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.tuvarna.bg.library to javafx.fxml;
+    exports com.tuvarna.bg.library;
+}
